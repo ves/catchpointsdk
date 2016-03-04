0b45681589cb247f045e512d22908c6690fa80a7
Vahram Sukyas <vahram@gmail.com>
2016-03-04 01:20:08 -0800

    leveldb backed auth token session store

--- auth.go
+++ auth.go
@@ -3,12 +3,18 @@ package catchpointsdk
 import (
   "net/http"
   "github.com/kelseyhightower/envconfig"
-  //"encoding/json"
+  "encoding/json"
   "fmt"
   "log"
   "bytes"
-  "io/ioutil"
+  "encoding/base64"
+  "github.com/syndtr/goleveldb/leveldb"
+  "time"
+  "strings"
+  "strconv"
 )
+
+const catchpointTokenExpireTime int = 1800
 
 type Authentication struct {
   ClientID string
@@ -16,26 +22,75 @@ type Authentication struct {
   Endpoint  string
 }
 
+type AuthResponse struct {
+  AccessToken string `json:"access_token"`
+  TokenType string `json:"token_type"`
+  ExpiresIn int `json:"expires_in"`
+}
+
+
 /*
-Authenticate into the Catchpoint API and return a Bearer token
+Returns the base64 encoded Catchpoint auth token
 */
-func Authenticate() {
+func Authenticate() string {
+  return checkToken()
+}
+
+/*
+HTTP request to get a new Catchpoint token; base64 encode the result
+*/
+func authToCatchpoint() (bearerToken string, accessToken int) {
+  fmt.Printf("calling catchpoint")
   var a Authentication
   err := envconfig.Process("catchpointsdk", &a)
   if err != nil { log.Fatal(err.Error()) }
-  fmt.Println(a.Endpoint)
-  b := []byte(`{"grant_type":"client_credentials","client_credentials":"%s","client_secret":"%s"}, a.ClientID, a.ClientSecret`)
+  b := []byte(fmt.Sprintf("grant_type=client_credentials&client_id=%s&client_secret=%s", a.ClientID, a.ClientSecret))
   req, err := http.NewRequest("POST", a.Endpoint, bytes.NewBuffer(b))
+  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+  req.Header.Add("Accept", "*/*")
   client := &http.Client{}
   resp, err := client.Do(req)
-  if err != nil {
-    panic(err)
-  }
+  if err != nil { panic(err) }
   defer resp.Body.Close()
 
-  fmt.Println("response Status:", resp.Status)
-  fmt.Println("response Headers:", resp.Header)
-  body, _ := ioutil.ReadAll(resp.Body)
-  fmt.Println("response Body:", string(body))
-  //return body
+  if resp.Status == "200 OK" {
+    decoder := json.NewDecoder(resp.Body)
+    var ar AuthResponse
+    _ = decoder.Decode(&ar)
+    bearerToken := base64.StdEncoding.EncodeToString([]byte(ar.AccessToken))
+    //log.Debug("Received Bearer token: %s - [base64 encoded] %s", ar.AccessToken, bearerToken)
+    return bearerToken, int(time.Now().Unix())
+  } else {
+    log.Fatal("Invalid response to authentication request received from Catchpoint")
+    panic(err)
+  }
+}
+
+/*
+Check to see if a valid Bearer token exists within the LevelDB store; if it
+does, return it, else authenticate to Catchpoint and store the token
+*/
+func checkToken() (token string) {
+  db, err := leveldb.OpenFile("catchpoint.state", nil)
+  if err != nil { panic(err) }
+  defer db.Close()
+  data, err := db.Get([]byte("token"), nil)
+  // no stored token found in LevelDB
+  if err != nil {
+    token, timeint := authToCatchpoint()
+    err = db.Put([]byte("token"), []byte(fmt.Sprintf("%v::%s", timeint, token)), nil)
+    if err != nil { panic(err) }
+  // there is a stored token; let's see if it's less than 1800 seconds old
+  } else {
+    s := strings.Split(string(data), "::")
+    timestring, token := s[0], s[1]
+    timeint, _ := strconv.Atoi(timestring)
+    diff := int(time.Now().Unix()) - timeint
+    // ask for a new token if we're within 10 seconds of token expiry
+    if diff > (catchpointTokenExpireTime - 10) {
+      token, timeint = authToCatchpoint()
+      err = db.Put([]byte("token"), []byte(fmt.Sprintf("%v::%s", timeint, token)), nil)
+    }
+  }
+  return token
 }
